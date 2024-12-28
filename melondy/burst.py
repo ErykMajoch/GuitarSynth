025fcaa8611cdcab75ea10d911ad9632e742f8f9
Eryk Majoch <eryk.majoch13@gmail.com>
2024-12-28 17:40:32 +0000

    Implemented BurstGenerator for random noise

---
+++ melondy/burst.py
@@ -0,0 +1,12 @@
+import numpy as np
+from typing import Protocol
+from melondy.temporal import Hertz
+
+
+class BurstGenerator(Protocol):
+    def __call__(self, num_samples: int, sampling_rate: Hertz) -> np.ndarray: ...
+
+
+class WhiteNoise:
+    def __call__(self, num_samples: int, sampling_rate: Hertz) -> np.ndarray:
+        return np.random.uniform(-1.0, 1.0, num_samples)
